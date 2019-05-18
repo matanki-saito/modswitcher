3c3d8f402317ad4c18f9bc3f5daaeed9ebf9c8d0
matanki.saito <matanki.saito@gmail.com>
2019-05-18 16:57:36 +0900

    移動

---
+++ starter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace starter
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //System.Net.WebClient wc = new System.Net.WebClient();
+            //wc.DownloadFile(url,"hoge.dll");
+            //wc.Dispose();
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "raballo.exe";
+
+            Process.Start(psi);
+        }
+    }
+}
